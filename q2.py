af53c88e311b07060bba115e2c414e36caed0083
Long Hoang <long@mindworker.de>
2016-10-07 00:38:12 +0800

    Check with lecture sample, found out counting is messed up

--- q2.py
+++ q2.py
@@ -1,6 +1,7 @@
 import csv
 import copy
 import math
+import numpy
 
 def normpdf(x, mean, sd):
     var = float(sd)**2
@@ -14,6 +15,8 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 isCont = ["new"]
+trainingFile = "data2.csv"
+sampleFile = "sample2.csv"
 
 className  = '' # last column will alwasy be the target class
 counters   = {}
@@ -27,7 +30,7 @@ def addCount(attr, val, cl):
         if cl not in cont[attr]:
             cont[attr][cl] = []
         else:
-            cont[attr][cl].append(val)
+            cont[attr][cl].append(float(val))
         return
         
 
@@ -48,7 +51,7 @@ def addCount(attr, val, cl):
         classCount[attr][val][cl] += 1
         
 # Read and prepare data
-with open('data.csv', 'rb') as csvfile:
+with open(trainingFile, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     firstRow = next(reader)
     className = firstRow[-1] # last column will alwasy be the target class
@@ -72,17 +75,29 @@ with open('data.csv', 'rb') as csvfile:
             addCount(iMap[i], elem, row[-1])
             i += 1
 
+    # calc mean and sd
+    for k in cont.keys():
+        for cl in cont[k].keys():
+            arr = numpy.array(cont[k][cl])
+            cont[k][cl] = {}
+            cont[k][cl]["mean"] = numpy.mean(arr, axis=0)
+            cont[k][cl]["std"] = numpy.std(arr, axis=0)
+                
     # todo remove
     pp.pprint(counters)
     pp.pprint(classCount)
+    print("Continous")
     pp.pprint(cont)
     
     
 # Calculate Prior
 # Use Hashtable to get O(1) access
 priors = {}
-total = float(len(counters[className]))
+total = 0.0
 
+for k in counters[className].keys():
+    total += counters[className][k]
+    
 for k in counters[className].keys():
     priors[k] = counters[className][k]/total
 
@@ -102,6 +117,9 @@ for attr in counters.keys():
         total = counters[attr][label]
 
         for cl in classCount[attr][label].keys():
+            print label + " " + cl + " " + str(classCount[attr][label][cl]),
+            print " " + str(total)
+
             cond[attr][cl] = classCount[attr][label][cl]/float(total)
 
 pp.pprint(cond)
@@ -109,7 +127,7 @@ pp.pprint(cond)
 # load sampe data
 data = {}
 contData = {}
-with open('sample.csv', 'rb') as csvfile:
+with open(sampleFile, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     firstRow = next(reader)
     
@@ -122,6 +140,7 @@ with open('sample.csv', 'rb') as csvfile:
             contData[attr] = []
         else:
             data[attr] = []
+            
         i += 1
 
     for row in reader:
@@ -136,4 +155,25 @@ with open('sample.csv', 'rb') as csvfile:
 
     # todo remove
     pp.pprint(data)
+    
+keys = data.keys()
+contKeys = contData.keys()
+numRows = len(data[keys[0]])
 
+for i in range(numRows):
+    print "----------------------------------------------------------------------"
+    print "Sample Row " + str(i)
+    print "----------------------------------------------------------------------"
+    for cl in counters[className].keys():
+        for k in keys:
+            val = data[k][i]
+            prop = cond[k][cl]
+
+            print "P( "+k+"='"+val+"' | "+className+"='"+cl+"' ) = \t"+str(prop)
+
+        for k in contKeys:
+            val = contData[k][i]
+            prop = normpdf(contData[k][i], cont[k][cl]["mean"], cont[k][cl]["std"])
+            
+            print "P( "+k+"="+val+" | "+className+"='"+cl+"' ) =   \t\t"+str(prop)
+    
