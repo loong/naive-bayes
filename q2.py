dbf4bd02b3c0c051bd5a3f9acff94e6a594fa19e
Long Hoang <long@mindworker.de>
2016-10-06 22:54:13 +0800

    Calculate condition probabilities

--- q2.py
+++ q2.py
@@ -9,19 +9,29 @@ className = '' # last column will alwasy be the target class
 iMap      = {} # indexMap
 data      = {} # @todo data might not be needed
 counters  = {}
+classCount = {}
 
-def addCount(attr, val, cl):
+def addCount(attr, label, cl):
     global counters
 
-    if val not in counters[attr]:
-        counters[attr][val] = 1
-    else:
-        counters[attr][val] += 1
+    if attr == "Name" or attr == "new":
+        return
 
-    if cl not in counters[attr+className]:
-        counters[attr+className][cl] = 1
+    if label not in counters[attr]:
+        counters[attr][label] = 1
     else:
-        counters[attr+className][cl] += 1
+        counters[attr][label] += 1
+
+    if attr == className:
+        return
+
+    if label not in classCount[attr]:
+        classCount[attr][label] = {}
+
+    if cl not in classCount[attr][label]:
+        classCount[attr][label][cl] = 1
+    else:
+        classCount[attr][label][cl] += 1
         
 # Read and prepare data
 with open('data.csv', 'rb') as csvfile:
@@ -30,15 +40,12 @@ with open('data.csv', 'rb') as csvfile:
     className = firstRow[-1] # last column will alwasy be the target class
     
     i = 0
-    for label in firstRow:
-        iMap[i] = label
-        data[label] = []
-        counters[label] = {}
-        counters[label+className] = {}
+    for attr in firstRow:        
+        iMap[i] = attr
+        data[attr] = []
+        counters[attr] = {}
+        classCount[attr] = {}
         i += 1
-
-    print(data)
-    print()
 
     for row in reader:
         i = 0
@@ -49,8 +56,9 @@ with open('data.csv', 'rb') as csvfile:
             i += 1
 
     # todo remove
-    pp.pprint(data)
     pp.pprint(counters)
+    pp.pprint(classCount)
+    
     
 # Calculate Prior
 # Use Hashtable to get O(1) access
@@ -64,8 +72,18 @@ print("Priors:")
 print(priors)
 
 # Calculate conditional probabilities
-# for cl in counters[className].keys(): # cl = class
-  #  if cl == "Name" or cl == className or cl == "new":
-        
+cond = {}
 
-        
+for attr in counters.keys():
+    if attr == "Name" or attr == className or attr == "new":
+        continue
+
+    cond[attr] = {}
+    
+    for label in counters[attr]:
+        total = counters[attr][label]
+
+        for cl in classCount[attr][label].keys():
+            cond[attr][cl] = classCount[attr][label][cl]/float(total)
+
+pp.pprint(cond)
