b8917bf481107e7bf9aa9bb0880ebda48890253c
Long Hoang <long@mindworker.de>
2016-10-06 23:20:25 +0800

    Extract continuous data (not using normal dist yet)

--- q2.py
+++ q2.py
@@ -1,37 +1,57 @@
 import csv
 import copy
+import math
+
+def normpdf(x, mean, sd):
+    var = float(sd)**2
+    pi = 3.1415926
+    denom = (2*pi*var)**.5
+    num = math.exp(-(float(x)-float(mean))**2/(2*var))
+    return num/denom
 
 # @todo remove
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-className = '' # last column will alwasy be the target class
-iMap      = {} # indexMap
-data      = {} # @todo data might not be needed
-counters  = {}
-classCount = {}
+ignore = ["Name"]
+isCont = ["new"]
 
-def addCount(attr, label, cl):
+className  = '' # last column will alwasy be the target class
+iMap       = {} # indexMap
+data       = {} # @todo data might not be needed
+counters   = {}
+classCount = {}
+cont       = {}
+
+def addCount(attr, val, cl):
     global counters
 
-    if attr == "Name" or attr == "new":
+    if attr in ignore:
         return
 
-    if label not in counters[attr]:
-        counters[attr][label] = 1
+    if attr in isCont:
+        if cl not in cont[attr]:
+            cont[attr][cl] = []
+        else:
+            cont[attr][cl].append(val)
+        return
+        
+
+    if val not in counters[attr]:
+        counters[attr][val] = 1
     else:
-        counters[attr][label] += 1
+        counters[attr][val] += 1
 
     if attr == className:
         return
 
-    if label not in classCount[attr]:
-        classCount[attr][label] = {}
+    if val not in classCount[attr]:
+        classCount[attr][val] = {}
 
-    if cl not in classCount[attr][label]:
-        classCount[attr][label][cl] = 1
+    if cl not in classCount[attr][val]:
+        classCount[attr][val][cl] = 1
     else:
-        classCount[attr][label][cl] += 1
+        classCount[attr][val][cl] += 1
         
 # Read and prepare data
 with open('data.csv', 'rb') as csvfile:
@@ -43,21 +63,30 @@ with open('data.csv', 'rb') as csvfile:
     for attr in firstRow:        
         iMap[i] = attr
         data[attr] = []
-        counters[attr] = {}
-        classCount[attr] = {}
+
+        if attr not in ignore:
+            if attr in isCont:
+                cont[attr] = {}
+            else:
+                counters[attr] = {}
+                classCount[attr] = {}
+            
         i += 1
 
     for row in reader:
         i = 0
         for elem in row:
-            print(elem)
             data[iMap[i]].append(elem)
             addCount(iMap[i], elem, row[-1])
+
+            if attr == "new":
+                cont
             i += 1
 
     # todo remove
     pp.pprint(counters)
     pp.pprint(classCount)
+    pp.pprint(cont)
     
     
 # Calculate Prior
